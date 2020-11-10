6b5b28d2e32cad18e2953fab0d03e0280f609159
poohnhi <poohnhi@gmail.com>
2020-11-10 12:34:11 +0700

    fix thuvien

--- QLNhaSach/QLNhaSach/ThuVien.cs
+++ QLNhaSach/QLNhaSach/ThuVien.cs
@@ -8,12 +8,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
-
-
+using QLNhaSach.Properties;
+using System.Reflection;
+using System.Resources;
 namespace QLNhaSach
 {
     public partial class ThuVien : Form
     {
+        ResourceManager rm = QLNhaSach.DataHinhAnh.ResourceManager;
         public ThuVien()
         {
             InitializeComponent();
@@ -41,101 +43,13 @@ namespace QLNhaSach
                 + Environment.NewLine
                 + Environment.NewLine + "- Nhà xuất bản: " + dt3.Rows[0][0];
         }
-
-        private void A001_Click(object sender, EventArgs e)
+        
+        private void Picture_Click(object sender, EventArgs e)
         {
-            SachTo.Image = global::QLNhaSach.DataHinhAnh.A001;
-            /*SachTo.Image = Image.FromFile(@"C:\Users\admin\Desktop\QLNS\Database\Pics. Ma Sach\A001.JPG");*/
-            LoadAnh("A001");
-        }
-
-        private void A002_Click(object sender, EventArgs e)
-        {
-            SachTo.Image = global::QLNhaSach.DataHinhAnh.A002;
-            LoadAnh("A002");
-        }
-
-        private void A003_Click(object sender, EventArgs e)
-        {
-            SachTo.Image = global::QLNhaSach.DataHinhAnh.A003;
-            LoadAnh("A003");
-        }
-
-        private void B001_Click(object sender, EventArgs e)
-        {
-            SachTo.Image = global::QLNhaSach.DataHinhAnh.B001;
-            LoadAnh("B001");
-        }
-
-        private void B002_Click(object sender, EventArgs e)
-        {
-            SachTo.Image = global::QLNhaSach.DataHinhAnh.B002;
-            LoadAnh("B002");
-        }
-
-        private void B003_Click(object sender, EventArgs e)
-        {
-            SachTo.Image = global::QLNhaSach.DataHinhAnh.B003;
-            LoadAnh("B003");
-        }
-
-        private void B004_Click(object sender, EventArgs e)
-        {
-            SachTo.Image = global::QLNhaSach.DataHinhAnh.B004;
-            LoadAnh("B004");
-        }
-
-        private void B005_Click(object sender, EventArgs e)
-        {
-            SachTo.Image = global::QLNhaSach.DataHinhAnh.B005;
-            LoadAnh("B005");
-        }
-
-        private void B006_Click(object sender, EventArgs e)
-        {
-            SachTo.Image = global::QLNhaSach.DataHinhAnh.B006;
-            LoadAnh("B006");
-        }
-
-        private void C001_Click(object sender, EventArgs e)
-        {
-            SachTo.Image = global::QLNhaSach.DataHinhAnh.C001;
-            LoadAnh("C001");
-        }
-
-        private void D001_Click(object sender, EventArgs e)
-        {
-            SachTo.Image = global::QLNhaSach.DataHinhAnh.D001;
-            LoadAnh("D001");
-        }
-
-        private void D002_Click(object sender, EventArgs e)
-        {
-            SachTo.Image = global::QLNhaSach.DataHinhAnh.D002;
-            LoadAnh("D002");
-        }
-        private void D003_Click(object sender, EventArgs e)
-        {
-            SachTo.Image = global::QLNhaSach.DataHinhAnh.D003;
-            LoadAnh("D003");
-        }
-
-        private void D004_Click(object sender, EventArgs e)
-        {
-            SachTo.Image = global::QLNhaSach.DataHinhAnh.D004;
-            LoadAnh("D004");
-        }
-
-        private void E001_Click(object sender, EventArgs e)
-        {
-            SachTo.Image = global::QLNhaSach.DataHinhAnh.E001;
-            LoadAnh("E001");
-        }
-
-        private void E002_Click(object sender, EventArgs e)
-        {
-            SachTo.Image = global::QLNhaSach.DataHinhAnh.E002;
-            LoadAnh("E002");
+            var picBox = (PictureBox)sender;
+            string a = picBox.Name;
+            SachTo.Image = (Image)rm.GetObject(a);
+            LoadAnh(a);
         }
     }
 }
