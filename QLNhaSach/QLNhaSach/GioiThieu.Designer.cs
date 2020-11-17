eeab017492a79debfce4fbfe7cc7c0ec33d76425
KiTi <hoangtien170919999@gmail.com>
2020-11-17 10:51:01 +0700

    demo 1

--- QLNhaSach/QLNhaSach/GioiThieu.Designer.cs
+++ QLNhaSach/QLNhaSach/GioiThieu.Designer.cs
@@ -35,11 +35,12 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(105, 69);
+            this.label1.Location = new System.Drawing.Point(99, 71);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(408, 25);
+            this.label1.Size = new System.Drawing.Size(279, 25);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Hoang Tien va Thien Y lam man hinh nay";
+            this.label1.Text = "Hoang Tien va Thien Y lam ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // GioiThieu
             // 
--- QLNhaSach/QLNhaSach/GioiThieu.cs
+++ QLNhaSach/QLNhaSach/GioiThieu.cs
@@ -16,5 +16,10 @@ namespace QLNhaSach
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
